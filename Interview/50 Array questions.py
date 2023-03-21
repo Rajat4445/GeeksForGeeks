9c3f7c15750249c5c6e4e79d9c53182e48dba64e
Rajat Bahuguna <rajatbahuguna4445@gmail.com>
2023-03-21 21:22:11 +0530

    Create 50 Array questions.py

---
+++ Interview/50 Array questions.py
@@ -0,0 +1,29 @@
+'''
+An element is called a peak element if its value is not smaller than the value of its adjacent elements(if they exists).
+Given an array arr[] of size N, Return the index of any one of its peak elements.
+Note: The generated output will always be 1 if the index that you return is correct. Otherwise output will be 0. 
+
+Example:
+Input: 
+N = 3
+arr[] = {1,2,3}
+Possible Answer: 2
+Generated Output: 1
+Explanation: index 2 is 3.
+It is the peak element as it is 
+greater than its neighbour 2.
+If 2 is returned then the generated output will be 1 else 0.
+'''
+
+class Solution:   
+    def peakElement(self,arr, n):
+
+        max_element = arr[0]     # Taking first element of array as max
+        
+        for i in range(len(arr)):       # iterating thorugh each element in array to get max_element out of the given array
+            if arr[i]>max_element:
+                max_element = arr[i]
+            else:
+                pass
+            
+        return arr.index(max_element)     # Fetching the index of max_element (first occurence)
