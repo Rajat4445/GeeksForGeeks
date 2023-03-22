c2d6a78882ad596cc8a6122b93528997943c36fd
Rajat Bahuguna <rajatbahuguna4445@gmail.com>
2023-03-22 22:19:47 +0530

    Update 50 Array questions.py

--- Interview/50 Array questions.py
+++ Interview/50 Array questions.py
@@ -27,3 +27,15 @@ class Solution:
                 pass
             
         return arr.index(max_element)     # Fetching the index of max_element (first occurence)
+    
+    
+'''
+Given an array A of size N of integers. Your task is to find the minimum and maximum elements in the array.
+
+Input:
+N = 6
+A[] = {3, 2, 1, 56, 10000, 167}
+Output:
+min = 1, max =  10000
+'''
+
