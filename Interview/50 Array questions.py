3ed85cbfb1ce08970c805a7c86d462e1aec9c906
Rajat Bahuguna <rajatbahuguna4445@gmail.com>
2023-03-22 22:56:24 +0530

    Update 50 Array questions.py

--- Interview/50 Array questions.py
+++ Interview/50 Array questions.py
@@ -97,3 +97,32 @@ class Solution:
     def sortArr(self, arr, n):      
                     
         return sorted(arr)                 ## Built-in function in python
+    
+'''
+Given a vector of N positive integers and an integer X. The task is to find the frequency of X in vector.
+
+ 
+
+Example 1:
+
+Input:
+N = 5
+vector = {1, 1, 1, 1, 1}
+X = 1
+Output: 
+5
+Explanation: Frequency of 1 is 5.
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
