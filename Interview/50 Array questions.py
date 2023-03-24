9ecb92c2dbd65977422cf506f069bcac7da65894
Rajat Bahuguna <rajatbahuguna4445@gmail.com>
2023-03-24 20:32:23 +0530

    Update 50 Array questions.py

--- Interview/50 Array questions.py
+++ Interview/50 Array questions.py
@@ -239,3 +239,29 @@ brr = arr.copy()
 arr[0] = brr[-1]
 arr[1:n] = brr[0:n-1]
 '''
+
+'''
+Given a String S, reverse the string without reversing its individual words. Words are separated by dots.
+
+Example 1:
+
+Input:
+S = i.like.this.program.very.much
+Output: much.very.program.this.like.i
+Explanation: After reversing the whole
+string(not individual words), the input
+string becomes
+much.very.program.this.like.i
+'''
+
+
+
+
+
+
+
+
+
+
+
+
