7c231249b111ee09cbb1cf8794df1ec027bad789
Rajat Bahuguna <rajatbahuguna4445@gmail.com>
2023-04-01 16:57:27 +0530

    Update 50 Array questions.py

--- Interview/50 Array questions.py
+++ Interview/50 Array questions.py
@@ -312,6 +312,30 @@ class Solution:
         return answer
 
 
-    
+'''
+Given an array a[] of size N which contains elements from 0 to N-1, you need to find all the elements occurring more than once in the given array.
+
+Note: The extra space is only for the array to be returned.
+Try and perform all operations within the provided array. 
+
+Example 1:
+
+Input:
+N = 4
+a[] = {0,3,1,2}
+Output: -1
+Explanation: N=4 and all elements from 0
+to (N-1 = 3) are present in the given
+array. Therefore output is -1.
+'''
+
+
+
+
+
+
+
+
+
 
 
