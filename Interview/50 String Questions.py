c046dee17e2606e9443c69fae8b097ee8bdd1f94
Rajat Bahuguna <rajatbahuguna4445@gmail.com>
2023-04-01 19:48:40 +0530

    Update 50 String Questions.py

--- Interview/50 String Questions.py
+++ Interview/50 String Questions.py
@@ -19,3 +19,26 @@ much.very.program.this.like.i
         output = '.'.join(reverse)
         
         return output
+    
+    
+'''
+
+'''
+class Solution:
+     
+    def longestCommonPrefix(self, arr, n):  # ex: arr = [geeksforgeeks, geeks, geek, geezer]
+        if not arr:    # Return -1 if list of strings is empty
+            return -1
+            
+        if n==1:       # Return the first element of the array as output if there is only one element in the array
+            return arr[0]
+    
+        prefix = ""    # Empty string
+    
+        for group in zip(*arr):         # in first iteration tuple = (g,g,g,g) second = (e,e,e,e) third = (e,e,e,e) and fourth = (e,e,e,k)
+            if len(set(group)) == 1:       # if the length of the set of tuple is 1 then only value in tuple gets to the prefix
+                prefix += group[0]
+            else:                       # Else loop breaks and the prefix is given as the output
+                break
+    
+        return prefix or -1             # IF there is a prefix value return it else give -1 as output
