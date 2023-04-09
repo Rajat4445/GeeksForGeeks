90ae8bca07114b674b348a58e7827ba08795592d
Rajat Bahuguna <rajatbahuguna4445@gmail.com>
2023-04-09 10:50:47 +0530

    Update 50 Array questions.py

--- Interview/50 Array questions.py
+++ Interview/50 Array questions.py
@@ -427,5 +427,19 @@ class Solution:
         return min_index
 
 
+'''
+Find the first non-repeating element in a given array arr of N integers.
+Note: Array consists of only positive and negative integers and not zero.
+
+Example 1:
+
+Input : arr[] = {-1, 2, -1, 3, 2}
+Output : 3
+Explanation:
+-1 and 2 are repeating whereas 3 is 
+the only number occuring once.
+Hence, the output is 3. 
+'''
+
 
 
