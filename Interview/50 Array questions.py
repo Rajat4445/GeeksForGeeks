8a727c832d1c8f43e82d2912eadb537f3cca9045
Rajat Bahuguna <rajatbahuguna4445@gmail.com>
2023-05-23 21:16:16 +0530

    Update 50 Array questions.py

--- Interview/50 Array questions.py
+++ Interview/50 Array questions.py
@@ -551,4 +551,20 @@ def rearrange(arr, n):
     return out
         
 
+'''
+Given an array of positive and negative numbers. Find if there is a subarray (of size at-least one) with 0 sum.
+
+Example 1:
+
+Input:
+5
+4 2 -3 1 6
+
+Output: 
+Yes
+
+Explanation: 
+2, -3, 1 is the subarray 
+with sum 0.
+'''
 
