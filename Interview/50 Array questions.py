e31e65967fe911dfacbdacdb1771c4b2b39c4965
Rajat Bahuguna <rajatbahuguna4445@gmail.com>
2023-06-10 11:27:07 +0530

    Update 50 Array questions.py

--- Interview/50 Array questions.py
+++ Interview/50 Array questions.py
@@ -227,6 +227,18 @@ class Solution:
 
         return arr
 
+'''
+Given an unsorted array arr[] of size N having both negative and positive integers. The task is place all negative element at the end of array without changing the order of positive element and negative element.
+
+Example 1:
+
+Input : 
+N = 8
+arr[] = {1, -1, 3, 2, -7, -5, 11, 6 }
+Output : 
+1  3  2  11  6  -1  -7  -5
+'''
+
 
 
 
